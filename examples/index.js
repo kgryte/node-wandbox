5e3d6bbb02994645763bc927d81aa327a336a9fe
kgryte <kgryte@gmail.com>
2016-03-16 00:33:08 -0700

    Update example

--- examples/index.js
+++ examples/index.js
@@ -1,23 +1,19 @@
 'use strict';
 
-var runWandbox = require( './../lib' );
-
-// String:
-
-var code = '#include <iostream>\nint main() {\n\tstd::cout << "All is well" << std::endl;}';
-runWandbox.fromString( code, clbk );
-
-// File:
-
-// Pass result to callback function...
-runWandbox( './examples/fixtures/code.cpp', clbk );
-
-// Save output to file...
-runWandbox( './examples/fixtures/output.json', './examples/fixtures/code.cpp', clbk );
+var wandbox = require( './../lib' );
 
 function clbk( error, results ) {
 	if ( error ) {
 		throw new Error( error.message );
 	}
 	console.log( results );
+	// returns {...}
 }
+
+// From file...
+wandbox( './examples/fixtures/code.cpp', clbk );
+
+// From string...
+var code = '#include <iostream>\nint main() {\n\tstd::cout << "All is well" << std::endl;}';
+
+wandbox.fromString( code, clbk );
